ba08fd56846fd76dd9de47243e9fc0912984f056
alekseev-ii <alekseev.ii@edu.spbstu.ru>
2025-11-10 12:56:38 +0300

    main done

--- main.cpp
+++ main.cpp
@@ -2,30 +2,43 @@
 
 struct IntArray {
   size_t size;
+  int * data;
+
+  IntArray(int n);
+  ~IntArray();
+
   void add(int next);
   int get(size_t id) const;
   int last() const;
   size_t get_size() const;
 }
 
-int main() {
+int main()
+{
   int next = 0;
   std::cin >> next;
-  IntArray a;
-  a.add(next);
+  try {
+    IntArray a(next);
 
-  while (std::cin >> next) {
-    a.add(next);
-  }
-  if (std::cin.fail()) {
-    return 1;
-  }
+    while (std::cin >> next) {
+      a.add(next);
+    }
+    if (std::cin.fail() && !std::cin.eof()) {
+      return 1;
+    } else if (std::cin.eof()) {
+      return 1;
+    }
 
-  size_t count = 1ull;
+    size_t count = 1ull;
 
-  for (size_t i = 0ull; i < a.size() - 1; ++i) {
-    int d = a.get(i)
-    count += !(d % a.last());
+    for (size_t i = 0ull; i < a.size() - 1; ++i) {
+      int d = a.get(i)
+      count += !(d % a.last());
+    }
+    std::cout << count << "\n";
+  } catch (const std::exception & e) {
+    return 2;
   }
-  std::cout << count << "\n";
 }
+
+
