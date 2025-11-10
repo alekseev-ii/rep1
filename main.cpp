ffe7dcb9c3932a78f933bbe3090395e0c1a86769
alekseev-ii <alekseev.ii@edu.spbstu.ru>
2025-11-10 12:39:14 +0300

    update smth

--- main.cpp
+++ main.cpp
@@ -1,1 +1,23 @@
-nt main() {}
+#include <iostream>
+
+struct IntArray {
+  size_t size;
+  void add(int next);
+  int get(size_t id) const;
+  size_t get_size() const;
+}
+
+int main() {
+  int next = 0;
+  std::cin >> next;
+  if (!std::cin) {
+    return 1;
+  }
+  IntArray a;
+  a.add(next);
+
+
+  for (size_t i = 0ull; i < a.size(); ++i) {
+    int d = a.get(i)
+  }
+}
