77fd9d1dcd681045da61a5d926cb0edc17cb1a60
alekseev-ii <alekseev.ii@edu.spbstu.ru>
2025-11-12 13:05:48 +0300

    fix moving and copying, some names

--- main.cpp
+++ main.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
 
 struct IntArray {
-  size_t k;
+  size_t size;
   int * data;
 
   IntArray(int i);
   ~IntArray();
+  IntArray & operator=(const IntArray& rhs);
+  IntArray(IntArray && rhs) noexcept;
+  IntArray & operator=(IntArray && rhs) noexcept;
 
   void add(int i);
   int get(size_t id) const noexcept;
   int last() const noexcept;
-  size_t size() const noexcept;
+  size_t get_size() const noexcept;
 };
 
 int main()
@@ -31,7 +34,7 @@ int main()
     if (!a.last()) {
       return 2;
     }
-    for (size_t i = 0ull; i < a.size() - 1; ++i) {
+    for (size_t i = 0ull; i < a.get_size() - 1; ++i) {
       int d = a.get(i);
       count += !(d % a.last());
     }
@@ -47,9 +50,36 @@ IntArray::~IntArray()
 }
 
 IntArray::IntArray(int i) : data(new int[1]),
-                            k(1)
+                            size(1)
 {
   data[0] = i;
+}
+
+IntArray & IntArray::operator=(const IntArray& rhs)
+{
+  if (this != &rhs) {
+    int * temp = new int[rhs.get_size()];
+    for (size_t i = 0ull; i < rhs.get_size(); ++i) {
+      temp[i] = rhs.get(i);
+    }
+    delete[] data;
+    data = temp;
+    size = rhs.get_size();
+  }
+}
+
+IntArray::IntArray(IntArray && rhs) noexcept : data(rhs.data), size(rhs.get_size())
+{
+  rhs.data = nullptr;
+}
+
+IntArray & IntArray::operator=(IntArray && rhs) noexcept
+{
+  delete[] data;
+  data = rhs.data;
+  size = rhs.get_size();
+  rhs.data = nullptr;
+  return *this;
 }
 
 int IntArray::get(size_t id) const noexcept
@@ -57,25 +87,25 @@ int IntArray::get(size_t id) const noexcept
   return data[id];
 }
 
-size_t IntArray::size() const noexcept
+size_t IntArray::get_size() const noexcept
 {
-  return k;
+  return size;
 }
 
 int IntArray::last() const noexcept
 {
-  return get( size() - 1);
+  return get( get_size() - 1);
 }
 
 void IntArray::add(int i)
 {
-  int * temp = new int[size() + 1];
-  for (size_t j = 0; j < size(); ++j) {
+  int * temp = new int[get_size() + 1];
+  for (size_t j = 0; j < get_size(); ++j) {
     temp[j] = get(j);
   }
-  temp[size()] = i;
+  temp[get_size()] = i;
   delete[] data;
   data = temp;
-  ++k;
+  ++size;
 }
 
