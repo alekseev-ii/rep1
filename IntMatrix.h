d16e2c1e6438b2a1f83dcde7fe29f4c0b929e43e
alekseev-ii <alekseev.ii@edu.spbstu.ru>
2025-11-19 15:27:53 +0300

    started main

---
+++ IntMatrix.h
@@ -0,0 +1,13 @@
+#ifndef INTMATRIX_H
+#define INTMATRIX_H
+#include <cstddef>
+
+struct IntMatrix {
+  size_t rows;
+  size_t cols;
+  IntArray data;
+
+
+};
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
-#include "IntArray.h"
+#include <fstream>
+#include "IntMatrix.h"
 
 
-int main()
+int main(int argc, char ** argv)
 {
+  std::ifstream input(argv[1]);
+  IntMatrix matrix;
+  try {
+    matrix.read(input);
+  } catch (std::invalid_argument &e) {
+    return 1;
+  } catch (std::bad_alloc &e) {
+    return 2;
+  }
+  input.close();
+
+  while (std::cin) {
+    int command = 0, p1 = 0, p2 = 0;
+    std::cin >> command >> p1 >> p2;
+    if (std::cin.fail() && !std::cin.eof()) {
+      return 1;
+    }
+    if (command == 1) {
+      matrix.insert_row(p1, p2);
+    } else if (command == 2) {
+      matrix.insert_column(p1, p2);
+    } else if (command == 3) {
+      matrix.insert_row(p1);
+      matrix.insert_column(p2);
+    }
+  }
 }
 
