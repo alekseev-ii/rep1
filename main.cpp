79094bca2eceb068b7b641b365ec8bda5cfc0fc3
alekseev-ii <alekseev.ii@edu.spbstu.ru>
2025-11-24 14:02:34 +0300

    add checks of sizes, remove command = 1

--- main.cpp
+++ main.cpp
@@ -20,18 +20,32 @@ int main(int argc, char ** argv)
   matrix.print(std::cout);
 
   while (std::cin) {
-    int command = 0, p1 = 0, p2 = 0;
-    std::cin >> command >> p1 >> p2;
+    int command = 0;
+    size_t p1 = 0, p2 = 0;
+    std::cin >> command;
+    std::cin >> p1 >> p2;
     if (std::cin.fail() && !std::cin.eof()) {
       return 1;
     }
-    if (command == 1) {
-      matrix.insert_row(p1, p2);
-    } else if (command == 2) {
+    if (command == 2) {
+      if (p1 > matrix.cols()) {
+        std::cerr << "bad column id" << "\n";
+        return 3;
+      }
       matrix.insert_column(p1, p2);
     } else if (command == 3) {
+      if (p1 > matrix.rows()) {
+        std::cerr << "bad row id" << "\n";
+        return 3;
+      } else if (p2 > matrix.cols()) {
+        std::cerr << "bad column id" << "\n";
+        return 3;
+      }
       matrix.insert_row(p1);
       matrix.insert_column(p2);
+    } else {
+      std::cerr << "bad command" << "\n";
+      return 3;
     }
     matrix.print(std::cout);
   }
